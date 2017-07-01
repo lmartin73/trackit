760810e666a6d8f785278cf55768cb23b2c42ee8
Jordan Hubbard <jordanhubbard@Jordans-MacBook-Pro.local>
2017-06-30 18:51:44 -0700

    Added `my organization` component and container and connected backend
    `my organization` lists out all of the associated organizations of the current user
    here, user will be able to see the list of organizations and click any organization
    (besides pending organizations) to see details about that organization.

---
+++ src/app/routes/organizations/components/ListOrganizations.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import { BigBreadcrumbs } from '../../../components'
+import JarvisWidget from '../../../components/widgets/JarvisWidget'
+
+/*
+    Dumb component for listing organizations current user is associated with
+*/
+export const ListOrganizations = (props) => {
+    return(
+        <div id="content" className="animated fadeInDown">
+            <div className="row">
+                <BigBreadcrumbs items={['Organizations', 'My Organizations']} icon="fa fa-fw fa-users"
+                                className="col-xs-12 col-sm-7 col-md-7 col-lg-4"/>
+            </div>
+            <div className="row col-lg-10 col-lg-offset-1">
+                <JarvisWidget colorbutton={true} editbutton={false} data-widget-fullscreenbutton="true" custombutton={false} data-widget-sortable="false">
+                    <header><h2>Organizations</h2></header>
+                    <div>
+                        <div className="widget-body">
+                            <div className="tabs-top">
+                                <ul className="nav nav-tabs tabs-left" id="demo-pill-nav">
+                                    <li className="active"><a href="#tab-r1" data-toggle="tab">Active</a></li>
+                                    <li><a href="#tab-r2" data-toggle="tab">Pending</a></li>
+                                </ul>
+                                <div className="tab-content">
+                                    <div className="tab-pane active" id="tab-r1"><br/><br/>
+                                        <div className="row">
+                                            {
+                                                (props.enrolledOrgs.length == 0) ? (
+                                                    <div>
+                                                        <p className="text-center text-default">No Active Organizations</p>
+                                                    </div>
+                                                ) : (
+                                                    props.enrolledOrgs.map(function(org) {
+                                                        return(
+                                                            <div key={org.id} className="col-lg-6 col-md-6 col-sm-6">
+                                                                <div className="panel panel-default">
+                                                                    <div className="form-inline">
+                                                                        <img src={org.logoURL} style={{objectFit: 'cover', height: "60px", width: "60px"}}/>
+                                                                        <button className="btn btn-link" onClick={() => props.orgClicked(org.id)}>{org.name}</button>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        )
+                                                    })
+                                                )
+                                            }
+                                        </div>
+                                    </div>
+                                    <div className="tab-pane" id="tab-r2"><br/><br/>
+                                        <div className="row">
+                                            {
+                                                (props.pendingOrgs.length == 0) ? (
+                                                    <div>
+                                                        <p className="text-center text-default">No Pending Organizations</p>
+                                                    </div>
+                                                ) : (
+                                                    props.pendingOrgs.map(function(org) {
+                                                        return(
+                                                            <div key={org.id} className="col-lg-6 col-md-6 col-sm-6">
+                                                                <div className="panel panel-default">
+                                                                    <div className="form-inline">
+                                                                        <img src={org.logoURL} style={{objectFit: 'cover', height: "60px", width: "60px"}}/>
+                                                                        <button className="btn btn-link" disabled>{org.name}</button>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        )
+                                                    })
+                                                )
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </JarvisWidget>
+            </div>
+        </div>
+    )
+}
---
+++ src/app/routes/organizations/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+/*
+    List of routes for organization components
+*/
+export default {
+    path: 'organizations',
+    component: require('../../components/common/Layout').default,
+    indexRoute: { onEnter: (nextState, replace) => replace('/organizations/listorgs') },
+    childRoutes: [
+        {
+            path: 'listorgs',
+            getComponent(nextState, cb){
+                System.import('./containers/ListOrgContainer.js').then((m)=> {
+                    cb(null, m.default)
+                })
+            }
+        },
+//        {
+//            path: 'detailorg',
+//            getComponent(nextState, cb){
+//                System.import('./containers/DetailOrgContainer.js').then((m)=> {
+//                    cb(null, m.default)
+//                })
+//            }
+//        },
+//        {
+//            path: 'editorg',
+//            getComponent(nextState, cb){
+//                System.import('./containers/EditOrg.js').then((m)=> {
+//                    cb(null, m.default)
+//                })
+//            }
+//        },
+//        {
+//            path: 'addorg',
+//            getComponent(nextState, cb){
+//                System.import('./containers/AddOrg.js').then((m)=> {
+//                    cb(null, m.default)
+//                })
+//            }
+//        },
+//        {
+//            path: 'joinorg',
+//            getComponent(nextState, cb){
+//                System.import('./containers/JoinOrg.js').then((m)=> {
+//                    cb(null, m.default)
+//                })
+//            }
+//        },
+    ]
+};
